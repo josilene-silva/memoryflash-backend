6766f8d17eeb6fd8dcd954932433cc33e24c863d
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-03-19 16:24:47 -0400

    feat: create error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import "reflect-metadata";
 import "express-async-errors";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import cors from "cors";
 
 import "@shared/container";
 import createConnection from "@shared/infra/database/typeorm";
 import { router } from "@shared/infra/http/routes";
+import { AppError } from "@shared/errors/AppError";
 
 createConnection();
 const app = express();
@@ -14,5 +15,20 @@ app.use(express.json());
 
 app.use(cors());
 app.use(router);
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 export { app };
