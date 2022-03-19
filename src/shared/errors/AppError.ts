a2d6417bc003a1638ffbda0e9b66a4b41ecad896
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-03-19 16:26:11 -0400

    refactor: class AppError

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,9 +1,12 @@
-export class AppError {
+export class AppError implements Error {
   public readonly message: string;
 
   public readonly statusCode: number;
 
+  public readonly name: string;
+
   constructor(message: string, statusCode = 400) {
+    this.name = "AppError";
     this.message = message;
     this.statusCode = statusCode;
   }
