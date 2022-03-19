24ac95e46a5d47a7dccbc0d31a709392d210500b
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-03-19 16:13:28 -0400

    feat: create class AppError

--- src/modules/accounts/useCases/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { CreateUserDTO } from "@modules/accounts/dtos";
 import { Users } from "@modules/accounts/infra/database/typeorm/entities/Users";
 import { IUsersRepository } from "@modules/accounts/repositories";
+import { AppError } from "@shared/errors/AppError";
 
 import { hash } from "bcrypt";
 import { injectable, inject } from "tsyringe";
@@ -16,7 +17,7 @@ class CreateUserUseCase {
     const userAlreadyExits = await this.usersRepository.findByEmail(email);
 
     if (userAlreadyExits) {
-      throw new Error("User already exists");
+      throw new AppError("User already exists");
     }
 
     const passwordHash = await hash(password, 10);
