50d535c3aeb5b990c4a9d7cef90f9973463b51c7
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-03-25 01:23:16 -0400

    fix: express types error

--- src/@types/express/index.ts
+++ src/@types/express/index.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/no-namespace */
 declare namespace Express {
   export interface Request {
     user: {
