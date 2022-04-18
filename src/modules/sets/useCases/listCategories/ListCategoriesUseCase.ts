47898a19f3529dbd3e65ebdf83107b922154fdff
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-04-18 01:49:53 -0400

    fix: remove redis cache

--- src/modules/sets/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/sets/useCases/listCategories/ListCategoriesUseCase.ts
@@ -8,22 +8,22 @@ import { inject, injectable } from "tsyringe";
 export class ListCategoriesUseCase {
   constructor(
     @inject("CategoriesRepository")
-    private categoriesRepository: ICategoriesRepository,
+    private categoriesRepository: ICategoriesRepository /* ,
     @inject("CacheProvider")
-    private cacheProvider: ICacheProvider
+    private cacheProvider: ICacheProvider */
   ) {}
 
   async execute(): Promise<Category[]> {
     let categories = [];
 
-    const categoriesCache = await this.cacheProvider.get("categories");
+    // const categoriesCache = await this.cacheProvider.get("categories");
 
-    if (!categoriesCache) {
-      categories = await this.categoriesRepository.list();
-      await this.cacheProvider.set(`categories`, JSON.stringify(categories));
-    } else {
-      categories = JSON.parse(categoriesCache);
-    }
+    // if (!categoriesCache) {
+    categories = await this.categoriesRepository.list();
+    //   await this.cacheProvider.set(`categories`, JSON.stringify(categories));
+    // } else {
+    //   categories = JSON.parse(categoriesCache);
+    // }
 
     return categories;
   }
