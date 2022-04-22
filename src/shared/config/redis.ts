f79764e6d327fd6f462f2c61585425e8eefa88f8
Josilene Vitoria dos Santos da Silva <josilenevitoriasilva@gmail.com>
2022-04-22 04:16:38 -0400

    fix: config env redis

--- src/shared/config/redis.ts
+++ src/shared/config/redis.ts
@@ -1,5 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const redisConfig = {
-  host: process.env.REDIS_HOST || "localhost",
-  port: Number(process.env.REDIS_PORT) || 6379,
-  password: process.env.REDIS_PASSWORD || "",
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD,
 };
